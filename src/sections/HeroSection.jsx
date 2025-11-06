9af094815b3699bac6572a5ff184102126a5702a
William Mattsson <william.mattsson@iths.se>
2025-11-06 20:02:18 +0100

    add CTA button to hero page and make it responsive

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -5,27 +5,38 @@ const HeroSection = () => {
     const sequence = roles.flatMap((role) => [role.text, 2000])
     return (
         <section id="hero">
-            <div className="hero-layout ">
-                <div className="hero-text flex-1">
-                    <p className="text-[36px] text-center">
-                        Hey, <span className="text-accent">I'm</span>
-                    </p>
-                    <h1 className="text-center">
-                        <span className="text-accent ">William</span> Mattsson
-                    </h1>
-                    <p className="text-[36px] text-center">
-                        <TypeAnimation
-                            sequence={sequence}
-                            wrapper="span"
-                            cursor={true}
-                            repeat={4}
-                            speed={30}
-                            style={{
-                                fontSize: '36px',
-                                display: 'inline-block'
-                            }}
-                        />
-                    </p>
+            <div className="hero-layout">
+                <div className="text-section flex flex-1 flex-col">
+                    <div className="hero-text ">
+                        <p className="text-[34px] font-normal ">
+                            Hey, <span className="text-accent">I'm</span>
+                        </p>
+                        <h1 className="">
+                            <span className="text-accent">William</span>{' '}
+                            Mattsson
+                        </h1>
+                        <p className="text-[36px]  font-semibold">
+                            <TypeAnimation
+                                sequence={sequence}
+                                wrapper="span"
+                                cursor={true}
+                                repeat={4}
+                                speed={30}
+                                style={{
+                                    fontSize: '40px',
+                                    display: 'inline-block'
+                                }}
+                            />
+                        </p>
+                    </div>
+                    <a
+                        href="#projects"
+                        className="bg-accent w-fit py-2 px-4 mx-auto mt-4 rounded-md hover:bg-accent-hover"
+                    >
+                        <div className="btn-container ">
+                            <span>See my work</span>
+                        </div>
+                    </a>
                 </div>
 
                 <div className="right-side flex-1">
