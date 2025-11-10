b617d0a73c00198b4e96dfdff589afbad96218d8
William Mattsson <william.mattsson@iths.se>
2025-11-10 15:55:35 +0100

    add header component with option to highligt a word

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from './components/Navbar'
+import SectionHeader from './components/SectioHeader'
 import FeatureCards from './sections/FeatureCards'
 import HeroSection from './sections/HeroSection'
 
@@ -8,6 +9,12 @@ const App = () => {
             <Navbar />
             <HeroSection />
             <FeatureCards />
+            <SectionHeader
+                title="Projects & Experience"
+                highlightWord="Experience"
+                sub="Check out my latest projects and tools"
+            />
+
             {/* Footer */}
         </main>
     )
---
+++ src/components/SectioHeader.jsx
@@ -0,0 +1,25 @@
+const SectionHeader = ({ title, sub, highlightWord }) => {
+    const titleParts = title.split(' ').map((word, index) => {
+        if (word === highlightWord) {
+            return (
+                <span key={index} className="text-accent">
+                    {word}
+                </span>
+            )
+        }
+        return word + ' '
+    })
+
+    return (
+        <div className="flex flex-col items-center gap-1">
+            <h2 className="font-medium md:text-4xl text-2xl">{titleParts}</h2>
+            {sub && (
+                <div>
+                    <p className="text-lg md:text-2xl font-extralight">{sub}</p>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default SectionHeader
