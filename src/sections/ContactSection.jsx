9b021cf20031840b56780bbcea33a14c6a503cfb
William Mattsson <william.mattsson@iths.se>
2025-11-14 13:23:22 +0100

    make contact section responsive

--- src/sections/ContactSection.jsx
+++ src/sections/ContactSection.jsx
@@ -3,20 +3,20 @@ import { contactLinks } from '../constants'
 
 const ContactSection = () => {
     return (
-        <section id="contact" className="mx-auto mt-30 w-[90%]">
+        <section id="contact" className="mx-auto mt-30 max-w-[90%]">
             <SectionHeader
                 title="Contact Me"
                 highlightWord="Contact"
                 sub="Feel free to reach out for projects"
             />
 
-            <p className="text-text-secondary bg-card p-8 text-center max-w-2xl mx-auto mt-8 rounded-xl card-border">
+            <p className="text-text-secondary bg-card p-3 md:p-8 text-center max-w-2xl mx-auto mt-8 rounded-xl card-border">
                 Are you in search of a talented frontend developer to bring your
                 ideas to life? Look no further! I'm William, and I specialize in
                 crafting engaging and user-friendly web applications.
             </p>
 
-            <div className="flex justify-center gap-6 bg-card card-border max-w-fit mx-auto rounded-xl p-6 mt-10">
+            <div className="flex justify-center gap-3 md:gap-6 bg-card card-border max-w-fit mx-auto rounded-xl p-4 md:p-6 mt-10">
                 {contactLinks.map((contact) => {
                     const Icon = contact.icon
                     return (
